2dcfd5e8512b1fdd07bb208d726f79900ec9173d
nayan malviya <naymalviya@gmail.com>
2025-11-12 21:05:22 +0530

    Update 12/11/2026

--- src/features/theme/themeSlice.jsx
+++ src/features/theme/themeSlice.jsx
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const themeSlice = createSlice({
   name: 'theme',
   initialState: {
-    value: false,  //Assuming this is a boolean for dark mode toggle
+    value: true,  //Assuming this is a boolean for dark mode toggle
   },
   reducers: {
     changeTheme: (state) => { state.value = !state.value; },
