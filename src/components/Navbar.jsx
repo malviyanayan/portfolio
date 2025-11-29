3c38dd075d002497b2187d54ea0fa056fa507199
nayan malviya <naymalviya@gmail.com>
2025-11-29 21:35:53 +0530

    Update Theme Button 29/11/25

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
             className={`d-flex h-100 align-items-center gap-2 fw-bold nav-logo ${
               theme ? "logo-light" : "logo-dark"
             }`}
-            style={{fontFamily:"'Playfair Display', sans-serif"}}
+            style={{ fontFamily: "'Playfair Display', sans-serif" }}
           >
             <i className="fa-solid fa-robot"></i>
             Nayan Malviya
@@ -91,16 +91,47 @@ const Navbar = () => {
               &times;
             </button>
 
-            {/* Theme Toggle */}
-            <label className="theme-switch">
+            <label className="switch">
+              <span className="sun">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <g fill="#ffd43b">
+                    <circle r="5" cy="12" cx="12"></circle>
+                    <path d="m21 13h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm-17 0h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm13.66-5.66a1 1 0 0 1 -.66-.29 1 1 0 0 1 0-1.41l.71-.71a1 1 0 1 1 1.41 1.41l-.71.71a1 1 0 0 1 -.75.29zm-12.02 12.02a1 1 0 0 1 -.71-.29 1 1 0 0 1 0-1.41l.71-.66a1 1 0 0 1 1.41 1.41l-.71.71a1 1 0 0 1 -.7.24zm6.36-14.36a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm0 17a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm-5.66-14.66a1 1 0 0 1 -.7-.29l-.71-.71a1 1 0 0 1 1.41-1.41l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.29zm12.02 12.02a1 1 0 0 1 -.7-.29l-.66-.71a1 1 0 0 1 1.36-1.36l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.24z"></path>
+                  </g>
+                </svg>
+              </span>
+
+              <span className="moon">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+                  <path d="m223.5 32c-123.5 0-223.5 100.3-223.5 224s100 224 223.5 224c60.6 0 115.5-24.2 155.8-63.4 5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6-96.9 0-175.5-78.8-175.5-176 0-65.8 36-123.1 89.3-153.3 6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"></path>
+                </svg>
+              </span>
+
+              <input
+                type="checkbox"
+                className="input"
+                onChange={() =>
+                  alert(
+                    "After the recent update, this feature is under construction. A better version is coming soon!"
+                  )
+                }
+              />
+
+              <span className="slider"></span>
+            </label>
+
+            {/* Theme Toggle - It is Old Version Off Theme Toggle and It Deprecated from 29/11/2025-------- */}
+            {/* <label className="theme-switch">
               <input
                 type="checkbox"
                 id="mode-toggle"
-                checked={!theme}
-                onChange={() => dispatch(changeTheme())}
+                // checked={!theme}
+                checked={theme}
+                onChange={() => alert("Sorry! After the recent update, this feature is under construction. A better version is coming soon")}
               />
               <span className="slider"></span>
-            </label>
+            </label> */}
+            {/* 29/11/2025--------  */}
           </div>
 
           {/* Sidebar Content Here */}
@@ -181,7 +212,13 @@ const Navbar = () => {
 
             <div className="action-buttons">
               <FadeInOnScroll>
-                <button onClick={() => window.open('https://drive.google.com/file/d/1-XNYsdXUf861wGJvqhNYMAkTxuFAC1JC/view?usp=sharing', '_blank')}
+                <button
+                  onClick={() =>
+                    window.open(
+                      "https://drive.google.com/file/d/1-XNYsdXUf861wGJvqhNYMAkTxuFAC1JC/view?usp=sharing",
+                      "_blank"
+                    )
+                  }
                   className={`custom-btn ${
                     theme ? "btn-light-mode" : "btn-dark-mode"
                   }`}
@@ -193,7 +230,8 @@ const Navbar = () => {
                 <button
                   className={`custom-btn ${
                     theme ? "btn-light-mode" : "btn-dark-mode"
-                  }`} onClick={() => window.location.href = '#contact-me'}
+                  }`}
+                  onClick={() => (window.location.href = "#contact-me")}
                 >
                   Contact Me
                 </button>
@@ -225,24 +263,24 @@ const Navbar = () => {
 
               <FadeInOnScroll>
                 <a
-                href="#projects"
-                className={`sidebar-link ${
-                  theme ? "sidebar-link-light" : "sidebar-link-dark"
-                }`}
-              >
-                <i className="fas fa-laptop-code me-2"></i> Projects
-              </a>
+                  href="#projects"
+                  className={`sidebar-link ${
+                    theme ? "sidebar-link-light" : "sidebar-link-dark"
+                  }`}
+                >
+                  <i className="fas fa-laptop-code me-2"></i> Projects
+                </a>
               </FadeInOnScroll>
-              
+
               <FadeInOnScroll>
                 <a
-                href="#skills"
-                className={`sidebar-link ${
-                  theme ? "sidebar-link-light" : "sidebar-link-dark"
-                }`}
-              >
-                <i className="fas fa-code me-2"></i> Skills
-              </a>
+                  href="#skills"
+                  className={`sidebar-link ${
+                    theme ? "sidebar-link-light" : "sidebar-link-dark"
+                  }`}
+                >
+                  <i className="fas fa-code me-2"></i> Skills
+                </a>
               </FadeInOnScroll>
             </div>
           </div>
